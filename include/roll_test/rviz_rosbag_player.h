bf315d02b9667d180208b39a4f7ad0f1dbd5fb1f
MatthewZid <m_zidianakis@hotmail.com>
2018-09-27 15:37:05 +0300

    Removed publisher to annotation panel

--- include/roll_test/rviz_rosbag_player.h
+++ include/roll_test/rviz_rosbag_player.h
@@ -51,8 +51,6 @@
 #include <boost/format.hpp>
 #include <mutex>
 
-#include <roll_test/RosbagMsgInfo.h>
-
 #define SECURE_OFFSET 10.0
 
 namespace rosbag
@@ -257,8 +255,6 @@ private:
 
     //psoitions of closest time frame to master topic
     std::vector<int> closest_pos_;
-
-    ros::Publisher msg_info_pub;
 
     //initial secure value of min_dist(minimum distance between master topic and candidate topic)
     double max_time_dist_;
